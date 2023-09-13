f5861d97e5cf29f2712ff5ef91608c37756e4327
tharu62 <deshantharindu.edirisinghe@mail.polimi.it>
2023-09-13 12:37:25 +0200

    aggiunta moveset

--- C++_SCRIPTS/Chess.cpp
+++ C++_SCRIPTS/Chess.cpp
@@ -1,6 +1,75 @@
 #include <iostream>
+#define INVALID -1;
 using namespace std;
 
+
+public bool[][] moveset(int row, int col){
+ 
+    if(row == 0 || row == 7){
+        switch (col)
+        {
+        // TORRE    
+        case 0:
+            
+            bool moveset[17][17];
+            for(int col = 0; col < 18; col++){
+                for(int row = 0; row < 18; row++){
+                    if(row == 8 || col == 8){
+                        moveset[col][row] = true;
+                    }else{
+                        moveset[col][row] = false;
+                    }
+                }
+            } 
+            return moveset;
+
+        // CAVALLO
+        case 1:
+            
+            bool moveset[17][17];
+            for(int col = 0; col < 18; col++){
+                for(int row = 0; row < 18; row++){
+                    // todo;
+                }
+            } 
+            return moveset;
+
+        // ALFIERE
+        case 2:
+            /* code */
+            break;
+
+        // RE
+        case 3:
+            /* code */
+            break;
+
+        // REGINA
+        case 4:
+            /* code */
+            break;
+
+        default:
+            break;
+        }
+
+    }else{
+
+        // PEDINA
+        bool moveset[17][17];
+        for(int col = 0; col < 18; col++){
+            for(int row = 0; row < 18; row++){
+                if((row == 7 || row == 6) && col == 8){
+                    moveset[col][row] = true;
+                }else{
+                    moveset[col][row] = false;
+                }
+            }
+        } 
+        return moveset;        
+
+    }
+}
 
 class Board{
     public:
