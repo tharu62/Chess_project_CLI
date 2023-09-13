9cbcc2fbfab5de43cc56bae675cd0d691b16966d
tharu62 <deshantharindu.edirisinghe@mail.polimi.it>
2023-09-13 13:59:21 +0200

    moveset finito

--- C++_SCRIPTS/Chess.cpp
+++ C++_SCRIPTS/Chess.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 
-public bool[][] moveset(int row, int col){
+bool** moveset(int row, int col){
+
+    bool** moveset = new bool*[17];
+    for (int i = 0; i < 17; ++i) {
+        moveset[i] = new bool[17];
+    }
  
     if(row == 0 || row == 7){
         switch (col)
@@ -11,13 +16,12 @@ public bool[][] moveset(int row, int col){
         // TORRE    
         case 0:
             
-            bool moveset[17][17];
-            for(int col = 0; col < 18; col++){
-                for(int row = 0; row < 18; row++){
+            for(int col = 0; col < 17; col++){
+                for(int row = 0; row < 17; row++){
                     if(row == 8 || col == 8){
-                        moveset[col][row] = true;
+                        moveset[row][col] = true;
                     }else{
-                        moveset[col][row] = false;
+                        moveset[row][col] = false;
                     }
                 }
             } 
@@ -26,27 +30,68 @@ public bool[][] moveset(int row, int col){
         // CAVALLO
         case 1:
             
-            bool moveset[17][17];
-            for(int col = 0; col < 18; col++){
-                for(int row = 0; row < 18; row++){
-                    // todo;
+            for(int col = 0; col < 17; col++){
+                for(int row = 0; row < 17; row++){
+                    if(((row == 6 || row == 10) && (col == 7 || col == 9)) || ((row == 7 || row == 9) && (col == 6 || col == 10))){
+                        moveset[row][col] = true;
+                    }else{
+                        moveset[row][col] = false;
+                    }
                 }
             } 
             return moveset;
 
         // ALFIERE
         case 2:
-            /* code */
-            break;
+            
+            for(int col = 0; col < 17; col++){
+                for(int row = 0; row < 17; row++){
+                    if(row == col || col == (16-row)){
+                        moveset[row][col] = true;
+                    }else{
+                        moveset[row][col] = false;
+                    }
+                }
+            } 
+            return moveset;
 
         // RE
         case 3:
-            /* code */
-            break;
+            
+            for(int col = 0; col < 17; col++){
+                for(int row = 0; row < 17; row++){
+                    if((row > 6 && row < 10) && (col > 6 && col < 10)){
+                        moveset[row][col] = true;
+                    }else{
+                        moveset[row][col] = false;
+                    }
+                }
+            } 
+            return moveset;
 
         // REGINA
         case 4:
-            /* code */
+            
+            for(int col = 0; col < 17; col++){
+                for(int row = 0; row < 17; row++){
+                    if(row == 8 || col == 8){
+                        moveset[row][col] = true;
+                    }else{
+                        if(row == col || col == (16-row)){
+                            moveset[row][col] = true;
+                        }else{
+                            
+                            if((row > 6 && row < 10) && (col > 6 && col < 10)){
+                                moveset[row][col] = true;
+                            }else{
+                                moveset[row][col] = false;
+                            }
+
+                        }
+                    }
+                }
+            } 
+            return moveset;
             break;
 
         default:
@@ -55,36 +100,70 @@ public bool[][] moveset(int row, int col){
 
     }else{
 
-        // PEDINA
-        bool moveset[17][17];
-        for(int col = 0; col < 18; col++){
-            for(int row = 0; row < 18; row++){
-                if((row == 7 || row == 6) && col == 8){
-                    moveset[col][row] = true;
-                }else{
+        if(row == 1 || col == 6){
+            // PEDINA
+            for(int col = 0; col < 17; col++){
+                for(int row = 0; row < 17; row++){
+                    if((row == 7 || row == 6) && col == 8){
+                        moveset[col][row] = true;
+                    }else{
+                        moveset[col][row] = false;
+                    }
+                }
+            } 
+            return moveset;
+        }else{
+
+            for(int col = 0; col < 17; col++){
+                for(int row = 0; row < 17; row++){
                     moveset[col][row] = false;
                 }
             }
-        } 
-        return moveset;        
+            return moveset; 
+
+        }
+                
 
     }
+
 }
 
-class Board{
-    public:
-        int grid[8][8];
-        int turn;
-};
+void print_moveset(bool** mat){
+    for(int col = 0; col < 17; col++){
+        for(int row = 0; row < 17; row++){
+            printf("%d ", mat[row][col]);
+            if(row == 16){
+                printf("\n");
+            }
+        }
+    } 
+}
 
 class Piece{
     public:
         string name;
-        bool moveset[8][8];
+        bool** moveset;
+};
+
+class Board{
+    public:
+        Piece grid[8][8];
+        int turn;
 };
 
 int main()
 {
-    cout << "ciao";
+    
+    Piece king;
+    king.name = "king";
+    king.moveset = moveset(0,3);
+
+    Piece player_1[16];
+    player_1[0] = king;
+    //printf("%s\n", player_1[0].name);
+    print_moveset(player_1[0].moveset);
+    
+    
     return 0;
+
 }
