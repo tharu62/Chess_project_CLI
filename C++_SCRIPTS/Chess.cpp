d06c9397d958427fdbd7fa13b8d77e7128c89b85
tharu62 <deshantharindu.edirisinghe@mail.polimi.it>
2023-09-13 15:24:45 +0200

    nameset completato

--- C++_SCRIPTS/Chess.cpp
+++ C++_SCRIPTS/Chess.cpp
@@ -2,6 +2,23 @@
 #define INVALID -1;
 using namespace std;
 
+class Piece{
+    public:
+        string name;
+        int row;
+        int col;
+        bool** moveset;
+};
+
+class Board{
+    public:
+        Piece grid[8][8];
+        int turn;
+};
+
+Board board;
+Piece player_1[16];
+Piece player_2[16];
 
 bool** moveset(int row, int col){
 
@@ -128,6 +145,79 @@ bool** moveset(int row, int col){
 
 }
 
+string nameset(int row, int col){
+
+    if(row == 0 || row == 7){
+
+        switch(col)
+        {
+        case 0:
+
+            return " rook ";
+
+        case 1:
+
+            return "horse ";
+
+        case 2:
+
+            return "bishop";
+
+        case 3:
+
+            return " king ";
+        
+        case 4:
+
+            return "queen ";
+
+        case 5:
+
+            return "bishop";
+
+        case 6:
+
+            return " horse";
+
+        case 7:
+
+            return " rook ";    
+
+        default:
+            break;
+        }
+
+    }else{
+
+        if(row == 1 || row == 6){
+            return " pawn ";
+        }else{
+            return " void ";
+        }
+    }
+    
+
+}
+
+void init_board(){
+
+    for(int col = 0; col < 8; col++){
+
+        for(int row = 0; row < 8; row++){
+
+            board.grid[col][row].name = nameset(row, col);
+            board.grid[col][row].moveset = moveset(row, col);
+
+        }
+
+    }
+
+}
+
+void make_a_move(int player, Piece piece){
+
+}
+
 void print_moveset(bool** mat){
     for(int col = 0; col < 17; col++){
         for(int row = 0; row < 17; row++){
@@ -139,30 +229,33 @@ void print_moveset(bool** mat){
     } 
 }
 
-class Piece{
-    public:
-        string name;
-        bool** moveset;
-};
+void print_board(Piece mat[8][8]){
 
-class Board{
-    public:
-        Piece grid[8][8];
-        int turn;
-};
+    for(int col = 0; col < 8; col++){
+        for(int row = 0; row < 8; row++){
+            cout << " " << mat[row][col].name;
+            if(row == 7){
+                printf("\n");
+            }
+        }
+    }
+
+}
 
 int main()
 {
-    
+
     Piece king;
     king.name = "king";
     king.moveset = moveset(0,3);
 
-    Piece player_1[16];
+    //printf("%s", king.name);
+
     player_1[0] = king;
     //printf("%s\n", player_1[0].name);
-    print_moveset(player_1[0].moveset);
-    
+    //print_moveset(player_1[0].moveset);
+    init_board();
+    print_board(board.grid);
     
     return 0;
 
